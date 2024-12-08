1ddde69176028239fb6c4da3d35f830949b208cd
Aaron Ager <aaronager369@gmail.com>
2024-12-08 08:52:19 -0500

    Added nvim profiles, updated zshrc and added a run once script (that hopefully works) with chezmoi to install zinit and reduce bloat in .zshrc

---
+++ dot_config/nvim-notetaking/lua/vim-cmds.lua
@@ -0,0 +1,42 @@
+---General---
+vim.cmd("syntax on")--enables syntax higlighting
+
+vim.opt.showtabline = 2--Always enable tab line
+
+vim.opt.wrap = false--Disable line wrapping
+
+vim.opt.sessionoptions = 'curdir,folds,globals,help,tabpages,terminal,winsize'--Saves and restores tab layout, useful for tabby.nvim
+
+vim.cmd("set number")--adds line numbers
+vim.opt.relativenumber = true--shows line numbers relative to the current line
+
+
+--highlights the matched text pattern when searching
+vim.cmd("set incsearch")
+vim.cmd("set nohlsearch")
+vim.opt.spelllang = "en_ca"
+vim.opt.spell = true
+
+vim.opt.autoindent = true--Copy line indents to new line
+
+vim.opt.conceallevel = 1--For obsidan.nvim
+
+--Terminal Colours--
+vim.opt.background = "dark"--forces dark mode for themes
+
+
+vim.opt.termguicolors = true--forces terminal colours
+
+vim.opt.signcolumn = "yes"--Adds sign column to prevent text from shifting
+
+---Tabbing---
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.opt.clipboard:append("unnamedplus")--Force system clipboard as default
+
+---Mappings---
+vim.g.mapleader = " "--maps space to be the "leader" key
+

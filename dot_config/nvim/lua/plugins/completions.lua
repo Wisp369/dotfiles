b916635ca5cd613fa9444f576a4529b3f1eae9c8
Aaron Ager <aaronager369@gmail.com>
2025-02-14 22:12:31 -0500

    Reconfigured snippets to actually work

--- dot_config/nvim/lua/plugins/completions.lua
+++ dot_config/nvim/lua/plugins/completions.lua
@@ -1,47 +1,51 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
-  "L3MON4D3/LuaSnip",
-  dependencies = {
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
-  "onsails/lspkind.nvim"
-    }
-  },
-  {
-  "hrsh7th/nvim-cmp",
-  config = function()
-    -- Set up nvim-cmp.
-    local cmp = require("cmp")
-    require("luasnip.loaders.from_vscode").lazy_load()
+    "hrsh7th/nvim-cmp",
+    priority = 100,
+    Lazy = false,
+    dependencies = {
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+    },
 
-    cmp.setup({
-      snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
-        --{ name = "nvim_lsp" },
-        { name = 'luasnip' }, -- For luasnip users.
-      }, {
-        { name = "buffer" },
-      }),
-    })
+    config = function()
+      -- Set up nvim-cmp.
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+      })
+      local ls = require "luasnip"
+      ls.config.set_config {
+        history = false,
+        updateevents = "TextChanged, TextChangedI"
+      }
       local lspkind = require("lspkind")
       cmp.setup {
         formatting = {
@@ -51,9 +55,10 @@ return {
             before = function(entry, vim_item)
               return vim_item
             end
-            })
-          }
+          })
         }
-  end
+      }
+    end
+
   }
 }

5df197bf6bf20fd4d686df274503a710479049d7
Aaron Ager <aaronager369@gmail.com>
2025-03-04 13:04:12 -0500

    Updated mason and removed pythondap (to be added again later)

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -18,18 +18,7 @@ return {
           function(config)
           require('mason-nvim-dap').default_setup(config)
           end,
-          python = function(config)
-            config.adapters = {
-              type = "executable",
-              command = "/usr/bin/python3",
-	            args = {
-		            "-m",
-		            "debugpy.adapter",
-	            },
-            }
-              require('mason-nvim-dap').default_setup(config)
-          end,
-            },
+        },
       })
     end,
   },

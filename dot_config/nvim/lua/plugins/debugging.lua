992ef6495af37a10791b55fd7636d2e54ccd43c4
Aaron Ager <aaronager369@gmail.com>
2025-05-19 21:03:10 -0400

    Called nvim-dapui setup

--- dot_config/nvim/lua/plugins/debugging.lua
+++ dot_config/nvim/lua/plugins/debugging.lua
@@ -1,32 +1,34 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-		vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
-		vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
-		vim.keymap.set("n", "<Leader>B", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<F5>", dap.continue, {})
-		vim.keymap.set("n", "<F10>", dap.step_over, {})
-		vim.keymap.set("n", "<F11>", dap.step_into, {})
-		vim.keymap.set("n", "<F12>", dap.step_out, {})
-		vim.keymap.set("n", "<Leader>R", dap.run_last, {})
+    require("dapui").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+    vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+    vim.keymap.set("n", "<Leader>B", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<F5>", dap.continue, {})
+    vim.keymap.set("n", "<F10>", dap.step_over, {})
+    vim.keymap.set("n", "<F11>", dap.step_into, {})
+    vim.keymap.set("n", "<F12>", dap.step_out, {})
+    vim.keymap.set("n", "<Leader>R", dap.run_last, {})
   end,
 }

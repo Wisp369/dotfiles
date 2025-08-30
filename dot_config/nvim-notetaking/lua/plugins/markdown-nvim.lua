b05fb705c40a7edb23bb39cfef71e61a17a80d0b
Wisp369 <70415783+Wisp369@users.noreply.github.com>
2025-08-30 12:43:21 -0400

    Updated render-markdown.nvim to only start when opening a markdown file and added default config options

--- dot_config/nvim-notetaking/lua/plugins/markdown-nvim.lua
+++ dot_config/nvim-notetaking/lua/plugins/markdown-nvim.lua
@@ -1,11 +1,10 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown" },
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ---@module "render-markdown"
-  ---@type render.md.UserConfig
-  opts = {
+  config = function()
     require('render-markdown').setup({
       completions = { blink = { enabled = true } },
     })
-  },
+  end
 }

0a240db2c68941513a79510242576c902a541002
Wisp369 <70415783+Wisp369@users.noreply.github.com>
2025-09-10 22:00:00 -0400

    Moved various keybinds to keymaps.lua and added 2 keybings for obsidian.nvim

--- dot_config/nvim-notetaking/lua/core/keymaps.lua
+++ dot_config/nvim-notetaking/lua/core/keymaps.lua
@@ -1,7 +1,15 @@
 ---Mappings---
-vim.g.mapleader = " "--maps space to be the "leader" key. Should be done BEFORE you load lazy
-local key = vim.api.nvim_set_keymap
-key("n", "<Tab>l", ":tabNext<cr>", { noremap = true })
-key("n", "<Tab>h", ":tabprevious<cr>", { noremap = true })
-key("n", "x", '"_x', { noremap = true })
-key("n", "X", '"_X', { noremap = true })
+vim.g.mapleader = " " --maps space to be the "leader" key. Should be done BEFORE you load lazy
+local map = vim.api.nvim_set_keymap
+map("n", "<Tab>l", ":tabNext<cr>", { noremap = true })
+map("n", "<Tab>h", ":tabprevious<cr>", { noremap = true })
+map("n", "x", '"_x', { noremap = true })
+map("n", "X", '"_X', { noremap = true })
+
+--- Obsidian.nvim ---
+map("n", "<leader>v", ":ObsidianPasteImg<CR>", { noremap = true })
+map("n", "<leader>T", ":ObsidianTemplate<CR>", { noremap = true })
+
+
+--- Autosession ---
+vim.keymap.set("n", "<leader>sl", ":SessionRestore<CR>", {})
--- dot_config/nvim-notetaking/lua/plugins/auto-session.lua
+++ dot_config/nvim-notetaking/lua/plugins/auto-session.lua
@@ -1,9 +1,8 @@
-return{
+return {
   'rmagatti/auto-session',
   lazy = false,
 
   opts = {
-    vim.keymap.set("n", "<leader>sl", ":SessionRestore", {}),
     suppressed_dirs = { '~/', '~/Downloads', '/' },
     auto_restore = false
   },

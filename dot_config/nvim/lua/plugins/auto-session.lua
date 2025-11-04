e6180e5305b5ba5ca73b5043628daeac79182719
Aaron Ager <aaronager369@gmail.com>
2025-11-04 16:11:10 -0500

    Updated auto-session.nvim in main nvim config

--- dot_config/nvim/lua/plugins/auto-session.lua
+++ dot_config/nvim/lua/plugins/auto-session.lua
@@ -3,7 +3,7 @@ return{
   lazy = false,
 
   opts = {
-    vim.keymap.set("n", "<leader>sl", ":SessionRestore", {}),
+    vim.keymap.set("n", "<leader>sl", ":AutoSession restore<CR>", {}),
     suppressed_dirs = { '~/', '~/Downloads', '/' },
     auto_restore = false
   },

624f57f76f26650107104dad0d44b06f6d74a60c
Aaron Ager <aaronager369@gmail.com>
2026-01-19 16:07:10 -0500

    removed final require lsp line in init.lua for main nvim profile

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("core.keymaps") --Loads keybinds, should be done BEFORE lazy is loaded to properly set the leader key
 require("core.options") --Loads vim options
 require("core.lazy") --Loads lazy
+require("core.lsp") -- Loads lsps
